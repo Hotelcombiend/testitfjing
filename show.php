ee0b033cc57607fc5077c80995aeb2db2d1dc534
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-11-27 12:04:36 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,7 +10,7 @@
 <body>
   <?php
   $conn = mysqli_init();
-  mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070141_databaseitf', 'WPFlhu83', 'it63070141_databaseitf', 3306);
+  mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070141_databaseitf', 'WPFlhu83', 'test', 3306);
   if (mysqli_connect_errno($conn))
   {
     die('Failed to connect to MySQL: '.mysqli_connect_error());

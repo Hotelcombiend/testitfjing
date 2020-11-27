418e576d39de49b8787be1d55363ec01bd20a472
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-11-27 12:05:09 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'databaseitf3.mysql.database.azure.com', 'yotsanan@databaseitf3', 'WPFlhu83', 'itflabtest', 3306);
+mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070141_databaseitf', 'WPFlhu83', 'test', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
